7301e17cd31cac6394bc775544b4e33ed107645c
AjKenz <ajibskenny@gmail.com>
2024-07-09 09:47:50 +0100

    additional update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import bottleBanner from '../assets/images/bannerBottle.png'
 
 function HomePage() {
     return (
         <div className='home-container'>
-            <Navbar />
+            <section className='header'>
+                <Navbar />
+                <div className='bottle-banner' />
+            </section>
         </div>
     )
 }
